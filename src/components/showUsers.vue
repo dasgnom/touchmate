f4e42405ace91541119744f2accb3a3fb630ef14
telegnom <max@telegnom.org>
2018-05-09 16:47:52 +0200

    [F] loads user images from gravatar if desired

--- src/components/showUsers.vue
+++ src/components/showUsers.vue
@@ -3,14 +3,27 @@
     <h2 class="mb-4">Users</h2>
     <div class="row">
       <div class="col-4 col-sm-3 col-md-2 tm-item-container" v-for="user in users" v-bind:key="user.id">
-        <router-link :to="'users/' + user.id">
-          <div class="tm-item">
-            <img class="img-fluid" src="src/assets/img/user.png" />
-            <div class="name">
-              {{ user.name }}
-            </div>
+        <div class="tm-item" v-on:click="$router.push(`users/${user.id}`)">
+          <b-img
+            v-if="user.avatar"
+            :src="'//localhost:8080/v3/images/' + user.avatar + '/img/'"
+            class="img-fluid"
+            style="max-width:150px; max-width:150px;" />
+          <b-img
+            v-if="!user.avatar && !$config.gravatar.use"
+            class="img-fluid"
+            style="max-width:150px; max-width:150px;" />
+          <b-img
+            v-if="!user.avatar && $config.gravatar.use"
+            src="/src/assets/img/user.png"
+            :src="user.email | gravatar"
+            class="img-fluid"
+            style="max-width:150px; max-width:150px;"
+            onerror="this.src='/src/assets/img/user.png'" />
+          <div class="name">
+            {{ user.name }}
           </div>
-        </router-link>
+        </div>
       </div>
     </div>
   </div>
